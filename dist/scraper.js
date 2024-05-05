e0ed51f733b4662381b777347c120d33675cd280
Eddy Dezuraud <eddy@gedivote.fr>
2024-05-05 14:23:16 +0200

    fix build

---
+++ dist/scraper.js
@@ -0,0 +1,99 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.scrapeImages = void 0;
+const utils_1 = require("./utils");
+const defaultOptions = {
+    limit: 10,
+    imgSize: '',
+    imgtype: '',
+    imgColor: '',
+    imgar: '',
+    fileType: '',
+    safe: false,
+    siteSearch: '',
+    rights: '',
+    metadata: true,
+    imgData: false
+};
+const scrapeImages = (query, options) => __awaiter(void 0, void 0, void 0, function* () {
+    if (!query)
+        throw new Error('Query is required');
+    if (options && options.limit && options.limit > 100)
+        throw new Error('Limit must be less than 100');
+    const queryOptions = Object.assign(Object.assign({}, defaultOptions), (options || {}));
+    const url = `https://www.google.com/search?as_st=y&as_q=${query}&as_epq=&as_oq=&as_eq=&imgsz=${queryOptions.imgSize}&imgar=${queryOptions.imgar}&imgcolor=${queryOptions.imgColor}&imgtype=${queryOptions.imgtype}&cr=&as_sitesearch=${queryOptions.siteSearch}&as_filetype=${queryOptions.imgtype}&tbs=${queryOptions.rights}&udm=2`;
+    const page = yield (0, utils_1.launchBrowserAndOpenPage)(url);
+    yield page.goto(url, { waitUntil: 'networkidle0' });
+    const button = yield page.$("#L2AGLb");
+    if (button) {
+        yield button.click();
+        yield page.waitForNavigation({
+            waitUntil: 'networkidle0',
+        });
+    }
+    yield page.setViewport({ width: 1920, height: 1080 });
+    yield (0, utils_1.scrollToEnd)(page);
+    // click on every .F0uyec element to open the image preview and get the src of the image preview (img.iPVvYb)
+    let elements = yield page.$$('.ob5Hkd');
+    if (options && options.random) {
+        elements = elements.sort(() => 0.5 - Math.random());
+    }
+    page.on('console', (msg) => __awaiter(void 0, void 0, void 0, function* () {
+        const msgArgs = msg.args();
+        for (let i = 0; i < msgArgs.length; ++i) {
+            console.log(yield msgArgs[i].jsonValue());
+        }
+    }));
+    const results = [];
+    for (let element of elements) {
+        if (queryOptions.limit && results.length >= queryOptions.limit)
+            break;
+        const imgSrc = yield element.$eval('img', (img) => img.src);
+        if (!imgSrc)
+            continue;
+        if (queryOptions.imgtype === 'photo' && !(0, utils_1.isPicture)(imgSrc))
+            continue;
+        element.click();
+        yield page.waitForSelector('.RfPPs', { visible: true });
+        yield page.waitForNetworkIdle();
+        const src = yield page.evaluate(() => {
+            const img = document.querySelector('img.sFlh5c.pT0Scc.iPVvYb');
+            const source = document.querySelector('a.Hnk30e.indIKd');
+            if (img) {
+                return {
+                    imgData: '',
+                    src: img.src,
+                    description: img.getAttribute('aria-label') || img.alt || img.title || '',
+                    source: source ? source.href : '',
+                    metadata: {}
+                };
+            }
+        });
+        if (src && src.src !== '') {
+            if (queryOptions.metadata || queryOptions.imgData) {
+                const { metadata, imgBuffer } = yield (0, utils_1.getImageData)(src.src);
+                src.imgData = queryOptions.imgData ? `data:image/${metadata.format};base64,${imgBuffer.toString('base64')}` : '';
+                if (queryOptions.metadata && metadata) {
+                    src.metadata.width = metadata.width || 0;
+                    src.metadata.height = metadata.height || 0;
+                    if (metadata.format) {
+                        src.metadata.format = metadata.format;
+                    }
+                }
+            }
+            results.push(src);
+        }
+    }
+    return results;
+});
+exports.scrapeImages = scrapeImages;
+//# sourceMappingURL=scraper.js.map
---
+++ dist/utils.js
@@ -0,0 +1,127 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getImageData = exports.launchBrowserAndOpenPage = exports.scrollToEnd = exports.sleep = exports.isPicture = void 0;
+const sharp_1 = __importDefault(require("sharp"));
+const puppeteer_1 = __importDefault(require("puppeteer"));
+const launchBrowserAndOpenPage = (url) => __awaiter(void 0, void 0, void 0, function* () {
+    const browser = yield puppeteer_1.default.launch({ headless: true });
+    const page = yield browser.newPage();
+    yield page.goto(url);
+    return page;
+});
+exports.launchBrowserAndOpenPage = launchBrowserAndOpenPage;
+const isScrollable = (page) => __awaiter(void 0, void 0, void 0, function* () {
+    return page.evaluate(() => {
+        return document.querySelector("#islmp input[type='button']") !== null;
+    });
+});
+const isButtonVisible = (page) => __awaiter(void 0, void 0, void 0, function* () {
+    return page.evaluate(() => {
+        function isVisible(e) {
+            return !!(e.offsetWidth || e.offsetHeight || e.getClientRects().length);
+        }
+        const button = document.querySelector("#islmp input[type='button']");
+        return button ? isVisible(button) : false;
+    });
+});
+const scrollToEnd = (page) => __awaiter(void 0, void 0, void 0, function* () {
+    const isScroll = yield isScrollable(page);
+    if (!isScroll) {
+        return;
+    }
+    return yield isButtonVisible(page);
+});
+exports.scrollToEnd = scrollToEnd;
+const getImageData = (imgSrc) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        let imgBuffer = Buffer.from(imgSrc, 'base64');
+        // deal with url images and base64 images
+        if (imgSrc.startsWith('http')) {
+            const response = yield fetch(imgSrc);
+            const buffer = yield response.arrayBuffer();
+            imgBuffer = Buffer.from(buffer);
+        }
+        else if (imgSrc.startsWith('data:image')) {
+            const uri = imgSrc.split(';base64,').pop();
+            if (!uri) {
+                throw new Error('Invalid base64 image');
+            }
+            imgBuffer = Buffer.from(uri, 'base64');
+        }
+        const metadata = yield (0, sharp_1.default)(imgBuffer).metadata();
+        return { metadata, imgBuffer };
+    }
+    catch (err) {
+        console.error(imgSrc, err);
+        throw err;
+    }
+});
+exports.getImageData = getImageData;
+const isPicture = (imgSrc) => __awaiter(void 0, void 0, void 0, function* () {
+    // analyser si l'image est une photo ou non en se basant sur le contenu de l'image nombre de couleurs, etc.
+    try {
+        let imgBuffer = Buffer.from(imgSrc, 'base64');
+        if (imgSrc.startsWith('data:image')) {
+            const uri = imgSrc.split(';base64,').pop();
+            if (!uri) {
+                throw new Error('Invalid base64 image');
+            }
+            imgBuffer = Buffer.from(uri, 'base64');
+        }
+        else {
+            // check if the image is supported image format
+            const supportedFormats = ['jpeg', 'webp', '.jpg'];
+            const format = imgSrc.split('.').pop();
+            if (!format || !supportedFormats.includes(format)) {
+                console.error('Unsupported image format');
+                return false;
+            }
+        }
+        const { data, info } = yield (0, sharp_1.default)(imgBuffer)
+            .raw()
+            .toBuffer({ resolveWithObject: true });
+        const width = info.width;
+        const height = info.height;
+        const uniqueColorsRow = new Set();
+        const uniqueColorsColumn = new Set();
+        for (let i = 0; i < width * 4; i += 4) {
+            const r = data[i];
+            const g = data[i + 1];
+            const b = data[i + 2];
+            uniqueColorsRow.add(`${r},${g},${b}`);
+        }
+        for (let i = 0; i < height * width * 4; i += width * 4) {
+            const r = data[i];
+            const g = data[i + 1];
+            const b = data[i + 2];
+            uniqueColorsColumn.add(`${r},${g},${b}`);
+        }
+        const limitColors = 50;
+        if (uniqueColorsRow.size < limitColors && uniqueColorsColumn.size < limitColors) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    catch (err) {
+        console.error(imgSrc, err);
+        throw err;
+    }
+});
+exports.isPicture = isPicture;
+const sleep = (ms) => new Promise(res => setTimeout(res, ms));
+exports.sleep = sleep;
+//# sourceMappingURL=utils.js.map
